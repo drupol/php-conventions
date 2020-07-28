ef95852b4d8ba8f5dbe9d24d145a188f8c288c97
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-07-28 11:52:42 +0200

    Update Grumphp extension and take in account the testsuites.

--- src/GrumphpTasksExtension.php
+++ src/GrumphpTasksExtension.php
@@ -11,6 +11,7 @@ use Symfony\Component\DependencyInjection\Exception\RuntimeException;
  *
  * 1: define extra tasks
  * 2: skip tasks
+ * 3: Update testsuites if needed
  */
 class GrumphpTasksExtension implements ExtensionInterface
 {
@@ -19,6 +20,35 @@ class GrumphpTasksExtension implements ExtensionInterface
      */
     public function load(ContainerBuilder $container)
     {
+        if ($container->hasParameter('skip_tasks')) {
+            $tasks = $container->getParameter('tasks');
+
+            foreach ($container->getParameter('skip_tasks') as $name) {
+                unset($tasks[$name]);
+            }
+
+            $container->setParameter('tasks', $tasks);
+
+            if ($container->hasParameter('testsuites')) {
+                $testsuites = $container->getParameter('testsuites');
+
+                foreach ($testsuites as $id => $data) {
+                    $testsuites[$id]['tasks'] = array_filter(
+                        $testsuites[$id]['tasks'],
+                        static function (string $task) use ($tasks): bool {
+                            return in_array($task, $tasks, true);
+                        }
+                    );
+
+                    if ($testsuites[$id]['tasks'] === []) {
+                        unset($testsuites[$id]);
+                    }
+                }
+
+                $container->setParameter('testsuites', $testsuites);
+            }
+        }
+
         if ($container->hasParameter('extra_tasks')) {
             $tasks = $container->getParameter('tasks');
 
@@ -34,16 +64,5 @@ class GrumphpTasksExtension implements ExtensionInterface
 
             $container->setParameter('tasks', $tasks);
         }
-
-      if ($container->hasParameter('skip_tasks')) {
-        $tasks = $container->getParameter('tasks');
-
-        foreach ($container->getParameter('skip_tasks') as $name) {
-          unset($tasks[$name]);
-        }
-
-        $container->setParameter('tasks', $tasks);
-      }
-
     }
 }
