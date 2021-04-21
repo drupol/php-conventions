7ae138fdfea2185f3e40597616ac697890739cd6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-04-21 20:52:35 +0200

    refactor: Add Psalm and License tasks.

--- config/php73/php_cs_fixer.config.php
+++ config/php73/php_cs_fixer.config.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE.txt file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 use drupol\PhpCsFixerConfigsPhp\Config\Php73;
 
-return Php73::create();
+$config = new Php73();
+$rules = $config->getRules();
+
+$rules['header_comment'] = [
+    'commentType' => 'PHPDoc',
+    'header' => trim(file_get_contents(__DIR__ . '/../../resource/header.txt')),
+    'location' => 'after_open',
+    'separate' => 'both',
+];
+
+return $config->setRules($rules);
--- config/psr12/php_cs_fixer.config.php
+++ config/psr12/php_cs_fixer.config.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE.txt file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 use drupol\PhpCsFixerConfigsPhp\Config\Psr12;
 
-return Psr12::create();
+$config = new Psr12();
+$rules = $config->getRules();
+
+$rules['header_comment'] = [
+    'commentType' => 'PHPDoc',
+    'header' => trim(file_get_contents(__DIR__ . '/../../resource/header.txt')),
+    'location' => 'after_open',
+    'separate' => 'both',
+];
+
+return $config->setRules($rules);
