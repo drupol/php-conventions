6f20022e0cd731dd88ad5118309b06383b0472df
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-02-03 09:15:48 +0100

    cs: Autofix code style

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 $config = require __DIR__ . '/config/php73/php_cs_fixer.config.php';
 
 $config
